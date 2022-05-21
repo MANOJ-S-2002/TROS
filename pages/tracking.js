d36466ca93629afa5060158c10357c21e5d70f14
MANOJ-S-2002 <manoj.23it@licet.ac.in>
2022-05-21 20:25:36 +0530

    updated

--- pages/tracking.js
+++ pages/tracking.js
@@ -1,19 +1,35 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import MapView from "react-native-maps";
+import {GOOGLE_MAPS_APIKEY} from "GoogleMapKey"
+import MapViewDirections from "react-native-maps-directions";
 
 const Tracking = () => {
+  const [state, setState] = useState({
+    pickupCords: {
+      latitude: 13.063327,
+      longitude: 80.234334,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+    droplocationCords: {
+      latitude: 11.4102,
+      longitude: 76.695,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+  });
+  const { pickupCords, droplocationCords } = state;
+
   return (
     <View style={styles.container}>
-      <MapView
-        style={StyleSheet.absoluteFill}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+      <MapView style={StyleSheet.absoluteFill} initialRegion={pickupCords}>
+        <MapViewDirections
+          origin={pickupCords}
+          destination={droplocationCords}
+          apikey={GOOGLE_MAPS_APIKEY}
+        />
+      </MapView>
     </View>
   );
 };
