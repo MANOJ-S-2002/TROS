e85ad4ae645d80fd0ee636ab2b8dfc3bfd3e039c
MANOJ-S-2002 <manoj.23it@licet.ac.in>
2022-05-21 19:17:28 +0530

    updated

--- pages/tracking.js
+++ pages/tracking.js
@@ -1,104 +1,27 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, SafeAreaView, Pressable } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import HelpData from "./HelppPageData";
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 
-
-const Tracking= () => {
-    const [answer, setAnswer] = useState(true);
-
-    return (
-        // <React.StrictMode>
-        <ScrollView>
-            <SafeAreaView style={styles.container}>
-                <Text style={styles.mainheader}>FAQ</Text>
-                <View>
-                    {HelpData.map(({ id, question, Answer }) => {
-                        return (
-                            <View key={id} style={styles.Listcontainer}>
-                                <View
-                                    style={{
-                                        flexDirection: "row",
-                                        justifyContent: "space-between",
-                                    }}
-                                >
-                                    <Text style={{ fontWeight: "bold" }}>
-                                        {question}
-                                    </Text>
-                                    <View style={styles.btn}>
-                                        <Pressable
-                                            onPress={() => {
-                                                setAnswer(!answer);
-                                            }}
-                                        >
-                                            {!answer ? (
-                                                <Ionicons name="add" size={25}></Ionicons>
-                                            ) : (
-                                                <Ionicons name="remove" size={25}></Ionicons>
-                                            )}
-                                        </Pressable>
-                                    </View>
-                                </View>
-                                <View style={styles.answer}>
-                                    {answer && <Text>{Answer}</Text>}
-                                </View>
-                            </View>
-                        );
-                    })}
-                </View>
-            </SafeAreaView>
-        </ScrollView>
-        // </React.StrictMode>
-    );
+const Tracking = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={StyleSheet.absoluteFill}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
+  );
 };
 
-export default Tracking;
-
 const styles = StyleSheet.create({
-    container: {
-        alignItems: "center",
-        margin: 10,
-        marginRight: 30,
-        marginLeft: 30,
-    },
-    mainheader: {
-        textAlign: "center",
-        marginTop: 20,
-        // fontSize: 20,
-    },
-    Listcontainer: {
-        justifyContent: "space-between",
-        marginTop: 30,
-        padding: 10,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-        backgroundColor: "white",
-        marginTop: 20,
-    },
-    btn: {
-        width: 25,
-        alignItems: "center",
-        height: 25,
-        justifyContent: "center",
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-        backgroundColor: "white",
-        borderRadius: 50,
-    },
-    answer: {
-        backgroundColor: "#f6f6f6",
-    },
+  container: {
+    flex: 1,
+  },
 });
+
+export default Tracking;
